912176773a1f6ae34069ebdc6e3ba8c666a2264d
Cobblestone <aidenhperry@gmail.com>
2024-01-01 19:12:17 -0500

    broken

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,9 @@
 <svelte:head>
 	<title>COBBLE</title>
   <meta name="description" content="I bet you've never seen a rock program! Fortunately for you, I exist. 🪨">
+  <meta property="og:title" content="COBBLE" />
+  <meta property="og:description" content="I bet you've never seen a rock program! Fortunately for you, I exist. 🪨" />
+  <meta property="og:url" content="https://" />
 </svelte:head>
 
 <Navbar />
