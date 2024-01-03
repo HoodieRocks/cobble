8cb69f47ecbfe5d81e27a09398a5c06c4edda48c
Cobblestone <aidenhperry@gmail.com>
2024-01-03 17:37:46 -0500

    🎱

---
+++ src/routes/8-ball/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import EightBall from "$lib/components/EightBall.svelte";
+	import { onMount } from "svelte";
+
+  onMount(() => {
+    const audio = new Audio("dramatic.mp3")
+    audio.play()
+  })
+</script>
+
+<main class="flex w-full h-screen items-center justify-center">
+  <div class="animate-eight">
+    <EightBall/>
+  </div>
+</main>
